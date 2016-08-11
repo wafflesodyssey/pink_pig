521f5630de9e108749f18370408967d63306e430
martin dally <martin.d.dally@gmail.com>
2016-08-11 13:31:43 -0400

    generated phone number table/tests/yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810204252) do
+ActiveRecord::Schema.define(version: 20160811172424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20160810204252) do
     t.integer  "person_id"
     t.string   "email"
     t.string   "email_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phone_numbers", force: :cascade do |t|
+    t.integer  "person_id"
+    t.string   "digits"
+    t.string   "phone_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
