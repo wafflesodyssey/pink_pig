193f3dc642bd3183e85edfe498941c88058ac3b7
Ava Evans <avaevansm@gmail.com>
2016-08-11 15:59:10 -0400

    created people controller with endpoints and views. created routes. added fave idon to tab. defined create/new methods in people controller.

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,14 @@
+class PeopleController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/people_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PeopleControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
+  test "create new person" do
+    jon = Person.create
+    assert jon
+  end
+
+end
