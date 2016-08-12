3e95025aa9b801e9c05640898ab338749fb46757
Erik Douglas <eadouglas10@gmail.com>
2016-08-12 14:43:24 -0400

    Made dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
   def show
-    #find inspirational quote
+    begin
+      @sales_name = current_user.name
+    rescue
+      @sales_name = "Erik"
+    end
     @follow_ups = Conversation.where(follow_up_on: Date.today)
+    @quote = Quote.offset(rand(Quote.count)).first
   end
 end
