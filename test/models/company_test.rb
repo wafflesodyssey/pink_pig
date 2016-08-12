e7370b62552bbc05414fe994bfd0b83b47a9b9cd
martin dally <martin.d.dally@gmail.com>
2016-08-12 10:54:35 -0400

    company and conversation associations test

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
 
-  test "true" do
-    assert true
-  end
-
   test "companies" do
     assert Company
+  end
+
+  test "has many conversations" do
+    company = companies(:piggy)
+    assert_equal 1, company.conversations.count
   end
 
 end
