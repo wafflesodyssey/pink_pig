cc8a2d8394669b8147ad86aca0fd018ab0939824
Ava Evans <avaevansm@gmail.com>
2016-08-12 13:51:58 -0400

    added show and create functions to people controller, wrote and ran tests.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,12 +3,25 @@ class PeopleController < ApplicationController
   end
 
   def show
+    @person = Person.find(params[:id])
   end
 
   def new
     @person = Person.new
   end
 
-  def edit
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to person_path(@person.id)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def person_params
+      params.require(:user).permit(:full_name, :company_id, :comments)
   end
 end
