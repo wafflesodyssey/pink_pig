56b72f0632eb82f53ba31a637bbf29e8b013f3ff
Lisa Jones <ljones4521@yahoo.com>
2016-08-13 13:18:27 -0400

    added permission to manager to update to mngr, created view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
   def create
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    User.find(params[:id]).update(role: "manager")
+    redirect_to users_path
   end
 end
