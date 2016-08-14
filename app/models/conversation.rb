125db1d60454ae736c5386a7308259a9ed01960d
Alex Shelton <tbfunkymonkey@gmail.com>
2016-08-14 16:15:03 -0400

    added validations for conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,4 +3,6 @@ class Conversation < ActiveRecord::Base
   belongs_to :person
   belongs_to :company
   validates :user_id, presence: true
+  validates :person_id, presence: true
+  validates :company_id, presence: true
 end
