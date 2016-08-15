ba6cffd08b315b3f2b8f53a29c93b5efdc58d591
Ava Evans <avaevansm@gmail.com>
2016-08-15 09:02:22 -0400

    added edit, update, and delete to controller

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -18,21 +18,36 @@ class PeopleControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    get :edit, id: 1
+    jon = people(:jon)
+    get :edit, id: jon.id
     assert_response :success
   end
 
-  # test "create new person" do
-  #   jon = Person.create(full_name: "jon", company_id: 2, comments: "idiot")
-  #   assert_equal jon.full_name, "jon"
-  # end
+  test "update person" do
+    @person = people(:jon)
+    response = patch :update, {id: @person.id, person: {title: "manager"}}
+    @person.reload
+    assert_equal "manager", @person.title
+    assert_response :redirect
+  end
+
 
   test "should create a new person" do
-    post :create, {user: {full_name: "jon", comments: "its a thing"}}
+    post :create, {person: {full_name: "jon", comments: "its a thing"}}
   end
 
   test "create person" do
     jon = Person.create(full_name: "jon",company_id: 2, comments: "idiot")
     assert_equal jon.full_name, "jon"
   end
+
+  test "delete person" do
+    @person = people(:jon)
+    response = delete :destroy, {id: @person.id}
+    assert_response :redirect
+    jon = Person.find_by(id: @person.id)
+    assert_equal jon, nil 
+  end
+
+
 end
